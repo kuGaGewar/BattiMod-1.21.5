f8ea6f4f4b0ae9c80820203eff4562696a09b76f
kuGaGewar <batti.schmidt@hotmail.com>
2025-05-22 15:23:01 +0200

    First Commit, init

---
+++ src/main/java/com/battimod/BattiMod.java
@@ -0,0 +1,16 @@
+package com.battimod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BattiMod implements ModInitializer {
+	public static final String MOD_ID = "battimod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/battimod/BattiModClient.java
@@ -0,0 +1,10 @@
+package com.battimod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BattiModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
